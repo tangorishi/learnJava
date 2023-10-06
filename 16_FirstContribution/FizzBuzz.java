f2a9d21f3c3656d1cae906b6c8569d44d4e431c3
ANKIT SAHA <85545712+Ankit-Saha08@users.noreply.github.com>
2023-10-06 14:30:06 +0530

    Create FizzBuzz.java
    Provided a comprehensive, easy solution for the FizzBuzz program in Java
    
    Signed-off-by: ANKIT SAHA <85545712+Ankit-Saha08@users.noreply.github.com>

---
+++ 16_FirstContribution/FizzBuzz.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FizzBuzz{
+  
+  public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter a number - ");      
+      int n = sc.nextInt();
+      List<String> result = fizzBuzz(n);
+      System.out.println("Resultant String Array" + result);
+  }
+
+  private static List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<String>();
+        for (int i=1; i<=n; i++){
+            if ((i%3 == 0) && (i%5 == 0))
+                list.add("FizzBuzz");
+            else if (i%3 == 0)
+                list.add("Fizz");
+            else if (i%5 == 0)
+                list.add("Buzz");
+            else
+                list.add(String.valueOf(i));
+        }
+        return list;
+  }
+}
